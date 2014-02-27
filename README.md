9e7b07ba2a1fbc45d1d4bd177cc1bc34d4c73ea4
rienierpatron <rienierpatron@gmail.com>
2014-02-27 20:51:53 +0800

    return to top plugin

---
+++ README.md
@@ -0,0 +1,27 @@
+TopIt - A Return to Top jQuery Plugin
+=====
+If scrolling back to top of your page seems to be a hassle, then here's one good plugin for you. This will let you return to the top of your page, without scrolling the scrollbar, by just a click.
+
+To use TopIt plugin:
+
+- Call the following files to your codes:
+
+    - css/topIt.css
+    - js/topIt/topIt.js
+
+- Create a div with a class "topIt" to create the scroll to top button.
+
+- Add the following to your script:
+
+        jQuery(document).ready( function($) {
+      		$('.topIt').topIt({
+      			label			: null, 	//default label "TOP"
+      			width			: null,		//default 35
+      			background		: null,		//default #000000
+      			border			: null,		//no border by default, sample format if will be used -> 1px solid black 
+      			color			: null,		//default #FFFFFF
+      			fontSize		: "10px",	//default 20px
+      			position		: null,		//default left
+      			startingDistance: null,		//default 100
+      		});
+      	});
---
+++ js/topIt/topIt.js
@@ -0,0 +1,95 @@
+(function($) {
+
+	$.fn.topIt = function( options ) {
+
+		var settings = $.extend({
+			label			: null, //label of the button
+			width			: null, //width of the button (min:35,max:300)
+			background		: null, //color of the button
+			border			: null,
+			color			: null, //color of the text
+			fontSize		: null, //text font size
+			position		: null, //positioning (left,right)
+			startingDistance: null, //distance of scroll where button will show
+			speed			: null,
+		},options);
+
+		return this.each( function() {
+			if(settings.label){
+				$(this).text(settings.label);
+			}else{
+				$(this).text("TOP");
+			}
+
+			if(settings.width){
+				if(settings.width >= 35 && settings.width <= 300){
+					$(this).css( 'width', settings.width+'px');
+				}
+				if(settings.width < 35){
+					$(this).css( 'width', '35px');
+				}
+				if(settings.width > 300){
+					$(this).css( 'width', '300px');
+				}
+			}else{
+				$(this).css( 'width', '35px');
+			}
+
+			if(settings.background){
+				$(this).css('background-color',settings.background);
+			}else{
+				$(this).css('background-color','#000000');
+			}
+
+			if(settings.color){
+				$(this).css('color',settings.color);
+			}else{
+				$(this).css('color','#FFFFFF');
+			}
+
+			if(settings.border){
+				$(this).css('border',settings.border);
+			}
+
+			if(settings.fontSize){
+				$(this).css('font-size',settings.fontSize);
+			}else{
+				$(this).css('font-size','20px');
+			}
+
+			if(settings.position){
+				if(settings.position == "right"){
+					$(this).css('right',0);
+				}
+			}
+			//scroll handler
+			$(window).scroll(function() {
+				if(settings.startingDistance){
+					distance = settings.startingDistance;
+				}else{
+					distance = 100;
+				}
+			    if ($(window).scrollTop() >= distance) {
+			    	if($(".topIt:hidden" ).length == 1){
+			    		$('.topIt').fadeIn();
+			    	}
+			    }else{
+			        $('.topIt').fadeOut();
+			    }
+			});
+
+			$(this).click(topIt);
+		});
+
+		function topIt(){
+			if(settings.speed){
+				speed = settings.speed;
+			}else{
+				speed = 100;
+			}
+			$("html").animate({ scrollTop: 0 }, speed);
+  			return false;
+		}
+	};
+
+}(jQuery));
